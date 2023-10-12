6972430c4bf213f597658d31ed92f9afc8a078c5
Ranjana Perera <76731513+aardperera@users.noreply.github.com>
2023-10-12 08:35:41 +0530

    edit app.module.ts file

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
-import { RatingModule } from 'ng-starrating';
+
 
 @NgModule({
   declarations: [
@@ -15,8 +15,7 @@ import { RatingModule } from 'ng-starrating';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RatingModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
