21c0a95b889a85f9cbaa628805077b7fb5ca3499
Ranjana Perera <76731513+aardperera@users.noreply.github.com>
2023-10-18 11:08:10 +0530

    Update cart.service.ts

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {Cart} from '../shared/models/Cart';
 import { Food } from '../shared/models/food';
 import { CartItem } from '../shared/models/cartItem';
@@ -7,7 +7,7 @@ import { CartItem } from '../shared/models/cartItem';
   providedIn: 'root'
 })
 export class CartService {
-  private cart:Cart =new Cart();
+  private cart:Cart = this.getCartFromLocalStorage();
   private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart);
 
   constructor() { }
@@ -18,11 +18,13 @@ export class CartService {
       return;
 
     this.cart.items.push(new CartItem(food));
+    this.setCartToLocalStorage();
 
   }
 
   removeFromCart(foodId: string):void{
     this.cart.items = this.cart.items.filter(item=>item.food.id ===foodId);
+    this.setCartToLocalStorage();
   }
 
   changeQuantity(foodId:string,quantity:number){
@@ -31,10 +33,29 @@ export class CartService {
 
     cartItem.quantity = quantity;
     cartItem.price = quantity* cartItem.food.price;
+    this.setCartToLocalStorage();
   }
 
   clearCart(){
     this.cart = new Cart();
+    this.setCartToLocalStorage();
   }
 
+  getCartObservable():Observable<Cart>{
+    return this.cartSubject.asObservable();
+  }
+
+  private setCartToLocalStorage():void{
+    this.cart.totalPrice = this.cart.items.reduce((prevSum, currentItem)=> prevSum+ currentItem.price,0);
+    this.cart.totalCount = this.cart.items.reduce((prevSum, currentItem)=>prevSum+currentItem.quantity,0);
+    const cartJson = JSON.stringify(this.cart);
+    localStorage.setItem('Cart',cartJson);
+    this.cartSubject.next(this.cart);
+
+  }
+
+  private getCartFromLocalStorage():Cart{
+    const cartJson=localStorage.getItem('Cart');
+    return cartJson? JSON.parse(cartJson): new Cart();
+  }
 }
