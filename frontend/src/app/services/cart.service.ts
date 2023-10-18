63da7d5f386b94b2855bfe2a58829c5ead6a4893
Ranjana Perera <76731513+aardperera@users.noreply.github.com>
2023-10-18 10:02:25 +0530

    Create cart.service.ts

---
+++ frontend/src/app/services/cart.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {Cart} from '../shared/models/Cart';
+import { Food } from '../shared/models/food';
+import { CartItem } from '../shared/models/cartItem';
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  private cart:Cart =new Cart();
+  private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart);
+
+  constructor() { }
+
+  addToCart(food:Food):void{
+    let cartItem = this.cart.items.find(item => item.food.id===food.id);
+    if (cartItem)
+      return;
+
+    this.cart.items.push(new CartItem(food));
+
+  }
+
+  removeFromCart(foodId: string):void{
+    this.cart.items = this.cart.items.filter(item=>item.food.id ===foodId);
+  }
+
+  changeQuantity(foodId:string,quantity:number){
+    let cartItem = this.cart.items.find(item=>item.food.id === foodId);
+    if(!cartItem) return;
+
+    cartItem.quantity = quantity;
+    cartItem.price = quantity* cartItem.food.price;
+  }
+
+  clearCart(){
+    this.cart = new Cart();
+  }
+
+}
